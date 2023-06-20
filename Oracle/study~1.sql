c57a4c1ca676536e7085c40aed828dad36f85725
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-06-20 17:51:05 +0900

    gg

---
+++ Oracle/study~1.sql
@@ -0,0 +1,8 @@
+--1. 춘 기술대학교의 학과 이름과 계열을 표시하시오. 단, 출력 헤더는 "학과 명", "계열" 으로 표시하도록 한다.
+ SELECT DEPARTMENT_NAME "학과 명", CATEGORY "계열"
+ FROM TB_DEPARTMENT
+
+--2. 학과의 학과 정원을 다음과 같은 형태로 화면에 출력한다.
+ SELECT 
+ FROM TB_DEPARTMENT
+ 
