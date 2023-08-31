63d61229d9bdb150183d85f100d56ded255ab282
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-08-31 17:40:13 +0900

    feat:2023.08.31일 수업

--- Oracle/spring.sql
+++ Oracle/spring.sql
@@ -8,3 +8,47 @@ CREATE TABLE BOARD(
 );
 
 SELECT*FROM BOARD;
+
+INSERT INTO board(no, title, content, writer)
+(SELECT SEQ_BOARD.NEXTVAL, title, content, writer FROM board);
+COMMIT;
+
+ALTER TABLE board ADD CONSTRAINT PK_BOARD PRIMARY KEY(no);
+
+SELECT * FROM board ORDER BY no DESC;
+
+-- 힌트..! (위에꺼보다 속도가 더 빠름)
+-- 첫페이지
+SELECT NUM, NO, TITLE, WRITER, REGDATE
+FROM (
+        SELECT /*+ INDEX_DESC(board PK_BOARD) */   
+                ROWNUM NUM, NO, TITLE, WRITER, REGDATE
+        FROM board
+        WHERE ROWNUM <= 10
+)
+WHERE NUM > 0;
+
+-- 두번째 페이지 (11~20)
+SELECT NUM, NO, TITLE, WRITER, REGDATE
+FROM (
+        SELECT /*+ INDEX_DESC(board PK_BOARD) */ 
+                ROWNUM NUM, NO, TITLE, WRITER, REGDATE
+        FROM board
+        WHERE ROWNUM <=20
+)
+WHERE NUM > 10;
+
+-- 세번째 페이지 (21~30)
+SELECT NUM, NO, TITLE, WRITER, REGDATE
+FROM (
+        --/*+ FULL(board PK_BOARD) */ --전부다 색출해내겠다 ASC도 쓸수 있음 쓴순서대로
+        SELECT /*+ INDEX_DESC(board PK_BOARD) */  --힌트(이상한 문법이들어가도 돌아감)보조적역할
+                ROWNUM NUM, NO, TITLE, WRITER, REGDATE
+        FROM board
+        WHERE ROWNUM <=30
+)
+WHERE NUM > 20;
+
+
+
+
