28e89572ea36eb13d2574475b38be8a919861881
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-09-05 17:46:14 +0900

    feat:2023.09.05일 수업

--- Oracle/spring.sql
+++ Oracle/spring.sql
@@ -64,4 +64,32 @@ SELECT  *FROM MEMBER;
 
 
 
+--------------------------------------------------------------------------------------------------------------------
+
+create table company (
+	vcode varchar2(10) primary key,
+	vendor  varchar2(20) not null
+);
+insert  into company values('10', '삼성');
+insert  into company values('20', '애플');
+create table Phone(
+	num varchar2(10) primary key,
+	model varchar2(20) not null,
+	price number not null,
+	vcode varchar2(10),
+   constraint fk_vcode foreign key(vcode) references company(vcode)
+);
+insert into Phone values('ZF01','Galaxy Z Flip5', 1369000,'10');
+insert into Phone values('S918N','Galaxy S23 Ultra', 1479000,'10');
+insert into Phone values('IPO02','iPhone 14',1250000,'20');
+create table userinfo (
+	id varchar(20) primary key,
+	pw varchar(20) not null
+);
+insert into userinfo values('member','member');
+insert into userinfo values('admin','admin');
+commit;
+
+
+
 
