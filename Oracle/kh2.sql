ca6a042eea6a8d04d1fc92118cb2b002ba512454
rladbtjd10 <133620500+rladbtjd10@users.noreply.github.com>
2023-10-26 14:04:23 +0900

    feat:2023.10.26

---
+++ Oracle/kh2.sql
@@ -0,0 +1,10 @@
+
+create table members(
+    id varchar2(20),
+    pwd varchar2(20),
+    email varchar2(50)
+);
+insert into members values('user1', 'user1', 'user1@email.com');
+commit;
+
+select*from members;
